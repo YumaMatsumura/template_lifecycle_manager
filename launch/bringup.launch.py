a452649cb38b3c689831fa9de7f75fe23bba76d6
YumaMatsumura <yumapine@gmail.com>
2022-07-02 22:58:37 +0900

    add container

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -1,19 +1,29 @@
+from launch_ros.actions import Node
 from launch_ros.actions import LoadComposableNodes
 from launch_ros.descriptions import ComposableNode
 from launch import LaunchDescription
 
 def generate_launch_description():
+    container = Node(
+        package='rclcpp_components',
+        executable='component_container'
+    )
+    
     components = LoadComposableNodes(
-        target_container='lifecycle_manager_container',
+        target_container='ComponentManager',
         composable_node_descriptions=[
             ComposableNode(
                 package='template_lifecycle_manager',
                 plugin='template_lifecycle_manager::LifecycleManager',
-                name='lifecycle_manager'
+                name='lifecycle_manager',
+                extra_arguments=[
+                    {'use_intra_process_comms': True}
+                ]
             )
-        ]
+        ], 
     )
     
     return LaunchDescription([
+        container,
         components
     ])
